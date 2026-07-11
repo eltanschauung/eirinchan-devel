6117a3c21af999ee6234b51a01aecc476b0ffe02
eltanschauung <xxlibtardeaterxx@gmail.com>
2026-07-11 18:01:01 -0400

    Remove unused root namespace module

--- lib/eirinchan.ex
+++
@@ -1,9 +0,0 @@
-defmodule Eirinchan do
-  @moduledoc """
-  Eirinchan keeps the contexts that define your domain
-  and business logic.
-
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
-end
