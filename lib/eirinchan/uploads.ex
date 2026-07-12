9d0c61bb2a7a9b533242a02dc29bcdf3c12f9e81
eltanschauung <xxlibtardeaterxx@gmail.com>
2026-07-12 19:14:59 -0400

    Allow FFmpeg's libwebp decoder

--- lib/eirinchan/uploads.ex
+++ lib/eirinchan/uploads.ex
@@ -12,7 +12,7 @@ defmodule Eirinchan.Uploads do
   @processed_extensions @image_extensions ++ @video_extensions
   @codec_whitelists %{
     ".avif" => "av1,libdav1d",
-    ".webp" => "webp",
+    ".webp" => "webp,libwebp",
     ".webm" => "vp8,vp9,av1,opus,vorbis",
     ".mp4" => "h264,av1,aac,opus,mp3"
   }
