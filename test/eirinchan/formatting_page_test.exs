2d40ccdc77aa77b00543546033e897db888491fe
eltanschauung <xxlibtardeaterxx@gmail.com>
2026-07-15 15:50:35 -0400

    Upgrade stored sticker image markup

---
+++ test/eirinchan/formatting_page_test.exs
@@ -0,0 +1,30 @@
+defmodule Eirinchan.FormattingPageTest do
+  use ExUnit.Case, async: true
+
+  alias Eirinchan.FormattingPage
+
+  test "upgrades stored sticker markup without altering unrelated images" do
+    html = """
+    <div>
+      <img src="/whalestickers/gojo.png" title="gojo">
+      <img src="https://example.com/unrelated.png" title="unrelated">
+    </div>
+    """
+
+    normalized =
+      FormattingPage.normalize_body(html, [
+        %{token: "gojo", path: "/whalestickers/gojo.png", width: 128, height: 130}
+      ])
+
+    {:ok, document} = Floki.parse_fragment(normalized)
+    [sticker] = Floki.find(document, ~s(img[src="/whalestickers/gojo.png"]))
+    [external] = Floki.find(document, ~s(img[src="https://example.com/unrelated.png"]))
+
+    assert Floki.attribute(sticker, "width") == ["128"]
+    assert Floki.attribute(sticker, "height") == ["130"]
+    assert Floki.attribute(sticker, "loading") == ["eager"]
+    assert Floki.attribute(sticker, "decoding") == ["async"]
+    assert Floki.attribute(external, "width") == []
+    assert Floki.attribute(external, "loading") == []
+  end
+end
