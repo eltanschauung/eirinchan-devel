946e861c3a0c287b835a18f5a57641c1bb946675
eltanschauung <xxlibtardeaterxx@gmail.com>
2026-07-16 13:55:42 -0400

    Match quick reply action font sizes

--- test/eirinchan_web/post_form_stylesheet_test.exs
+++ test/eirinchan_web/post_form_stylesheet_test.exs
@@ -12,4 +12,15 @@ defmodule EirinchanWeb.PostFormStylesheetTest do
                "#reply-form textarea[name=body],#reply-form [name=user_flag]" <>
                "{box-sizing:border-box;width:100%}"
   end
+
+  test "quick reply action controls share the same inherited font size" do
+    stylesheet =
+      :eirinchan
+      |> Application.app_dir("priv/static/stylesheets/eirinchan-public.css")
+      |> File.read!()
+
+    assert stylesheet =~
+             "#quick-reply input.quick-reply-action,body #quick-reply .quick-reply-spoiler," <>
+               "body #quick-reply .quick-reply-spoiler>label{font-size:inherit}"
+  end
 end
