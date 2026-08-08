044c68dbcbf2adbf9874252298f9a0a1e163fbb3
eltanschauung <xxlibtardeaterxx@gmail.com>
2026-08-08 18:27:09 -0400

    Make ban browser theme compatible

--- test/eirinchan_web/manage_stylesheet_test.exs
+++ test/eirinchan_web/manage_stylesheet_test.exs
@@ -22,4 +22,18 @@ defmodule EirinchanWeb.ManageStylesheetTest do
     assert stylesheet =~ "table.modlog tr th{background:#282a2e}"
     refute stylesheet =~ "table.modlog tr th{background:#EA8}"
   end
+
+  test "ban browser surfaces inherit the selected theme palette" do
+    stylesheet =
+      :eirinchan
+      |> Application.app_dir("priv/static/stylesheets/mod/ban-list.css")
+      |> File.read!()
+
+    assert stylesheet =~ "--banlist-surface: color-mix(in srgb, currentColor 7%, transparent)"
+    assert stylesheet =~ "background: var(--banlist-heading-surface)"
+    refute stylesheet =~ "#d9bfb7"
+    refute stylesheet =~ "background: #ea8"
+    refute stylesheet =~ "color: #800"
+    refute stylesheet =~ "rgba(240, 224, 214"
+  end
 end
